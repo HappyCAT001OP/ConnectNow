dab1bc8c5ba1456a8a4f4509599eef0459b7351b
ManojSalugari <146091847+ManojSalugari@users.noreply.github.com>
2025-06-12 17:02:48 +0530

    feat: re-initialize middleware with comprehensive Clerk config

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,25 @@
- 
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/sign-in",
+    "/sign-up",
+    "/api/webhook",
+    "/api/uploadthing",
+    "/api/trpc/uploadthing",
+    "/api/trpc/uploadthing.uploadthing",
+    "/api/trpc/uploadthing.uploadthing.uploadthing",
+  ],
+  ignoredRoutes: [
+    "/api/webhook",
+    "/api/uploadthing",
+    "/api/trpc/uploadthing",
+    "/api/trpc/uploadthing.uploadthing",
+    "/api/trpc/uploadthing.uploadthing.uploadthing",
+  ]
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+}; 
